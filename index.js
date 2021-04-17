6aad7b25d149fc7e70112815602b084ca381b5ca
Pranav <pkakadiya274@rku.ac.in>
2021-04-17 21:15:15 +0530

    old url for ref

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var port = process.env.PORT || 5000;
 
 // mongoose.connect("mongodb://localhost:27017/UserApi",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     //  mongodb+srv://pranav:<password>@cluster0.mnb30.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+    // mongoose.connect('mongodb://localhost:27017/UserApi',{ useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
 mongoose.connect("mongodb+srv://pranav:12345@cluster0.mnb30.mongodb.net/UserApi?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app=express();
